2b2cd770317da0d71586e6aec3b47b71260a6209
Zubair Ahmed <zahmad2812@gmail.com>
2024-12-28 19:16:34 +0500

    Added Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <h1>{`Hello, Next.js! Welcome to Zubair's website!`}</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">{`Hello, Next.js! Welcome to Zubair's website!`}</h1>
+  );
 }
